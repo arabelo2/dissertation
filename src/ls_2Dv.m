96f68d25195abc6a083abc3d416d5813f6ad72ae
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2019-01-26 22:13:25 -0200

    Create ls_2Dv.m and update rs_2Dv.m and workspace.m

--- src/ls_2Dv.m
+++ src/ls_2Dv.m
@@ -1,9 +1,9 @@
-function p = rs_2Dv(b, f, c, e, x, z, varargin)
+function p = ls_2Dv(b, f, c, e, x, z, varargin)
 
 % Compute wave number
 kb = 2*pi.*b.*f/c;
 
-% If a number of segments is specified, use
+% If number of segments is specified, use
 if nargin == 7
     N = varargin{1};
 else
@@ -19,24 +19,24 @@ zb = z./b;
 eb = e./b;
 
 % Compute normalized centroid locations for the segments
-xc = zeros(1, N);
+xc = zeros(1,N);
 for jj = 1:N
     xc(jj) = -1 + 2*(jj - 0.5)/N;
 end
 
 % Calculate normalized pressure as a sum over all the segments as an
-% approximation of the Rayleigh-Somerfeld type of integral
+% approximation of the Rayleigh-Sommerfeld tye of integral
 
 p = 0;
 for kk = 1:N
+    ang = atan((xb - xc(kk) - eb)./zb);
+    ang = ang + eps.*(ang == 0);
+    dir = sin(kb.*sin(ang)/N)./(kb.*sin(ang)/N);
     rb = sqrt((xb - xc(kk) - eb).^2 + zb.^2);
-    p  = p + besselh(0, 1, kb.*rb);
-    plot(rad2deg(asin(x/z)), abs(p).*(kb./N))
-    hold off
-    pause(.1)
+    ph = exp(i*kb.*rb);
+    p = p + dir.*exp(i*kb.*rb)./sqrt(rb);
 end
-
-p = p.*(kb./N); % Include external factor
+p = p.*(sqrt(2*kb./(i*pi)))/N; % include external factor
 
 % Reference
 % Jr, Lester W. Schmerr. Fundamentals of Ultrasonic Phased Arrays. Solid Mechanics and Its Applications. Springer International Publishing, 2015. //www.springer.com/us/book/9783319072715.
--- src/workspace.m
+++ src/workspace.m
@@ -1,22 +1,39 @@
-f = 5e6
+f = 2.25e6
 c = 1500
-b = 0.3/2
-e = b/100
-x = 0
-z = 0:100
+b = 19e-3/2
+e = .05*b
+x = (-40:0.1:40)*1e-3;
+z = (40)*1e-3
 
-lambda = c/f;
+lambda = c/f
 
-if 2*b*1e-3 > lambda/10
-    Nopt = ceil(20e3*f*b/c);
+if 2*b > lambda/10
+    Nopt = ceil(20*f*b/c);
 else
-    Nopt = 1
+    Nopt = 1;
 end
 
 Nopt
-2e-3*b/lambda
+
+% p = rs_2Dv(b, f, c, e, x, z, Nopt);
+% plot(z, abs(p))
+% hold on
+% plot(z, abs(p), 'ko')
+% hold off
+
 
 p = rs_2Dv(b, f, c, e, x, z, Nopt);
-plot(z, abs(p))
+plot(rad2deg(asin(x/z)), abs(p))
 hold on
-plot(z, abs(p), 'ro')
+plot(rad2deg(asin(x/z)), abs(p), 'ko')
+hold off
+
+% p = cell(size(x));
+% for xx = 1:length(x)
+%     p{xx} = rs_2Dv(b, f, c, e, x(xx), z, Nopt);
+% end
+% 
+% for xx = 1:length(x)
+%     plot(z, abs(p{xx}))
+%     pause(.5)
+% end
