7902dd06f38968b154301e852e5ef13675e963b1
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2019-01-28 00:47:29 -0200

    Update loading.m

--- src/loading.m
+++ src/loading.m
@@ -1,18 +1,18 @@
 tic()
 f0 = 5e6;
-c = 1480;
+c = 1500;
 width = 6e-3/2;
 elements_x = 1;
 height = 2*width;
 elements_y = 1;
 kerf = 4e-4;
 lambda = c/f0;
-xmin = -15e-3; %-(3*width + kerf) * (elements_x/2+1);
-xmax = +15e-3; %(3*width + kerf) * (elements_x/2+1);
+xmin = -25e-3; %-(3*width + kerf) * (elements_x/2+1);
+xmax = +25e-3; %(3*width + kerf) * (elements_x/2+1);
 ymin = 0;
 ymax = 0;
 zmin = 0;
-zmax = +80e-3; % 400 * lambda;
+zmax = 100e-3; % 400 * lambda;
 focus_x = 0;
 focus_y = 0;
 focus_z = 25 * lambda;
@@ -27,7 +27,7 @@ y = ymin:dy:ymax;
 z = zmin:dz:zmax;
 
 % x = 0;
-% z  = 60e-3;
+% z  = 15e-3;
 
 if 2*width > lambda/10
     Nopt = ceil(20*f0*width/c);
@@ -67,14 +67,33 @@ P2 = cell(length(z), length(x));
 % % set(gca,'FontSize',20);
 
 
-mat = [1000, 1500, 1000, 1500];
-angt = 0;
-Dt0 = 1e-3;
+mat = [997, 1480, 997, 1480];
+angt = 10.217;
+Dt0 = 50.8e-3;
 p = ls_2Dint(width, f0, mat, kerf, angt, Dt0, x, z, Nopt);
 
 for xx = 1:length(x)
     for zz = 1:length(z)
-        P1{zz, xx} = rs_2Dv(width, f0, c, kerf, x(xx), z(zz), Nopt);
-        P2{zz, xx} = ls_2Dint(width, f0, mat, kerf, angt, Dt0, x, z, Nopt);
+        P1{xx, zz} = rs_2Dv(width, f0, c, kerf, x(xx), z(zz), Nopt);
+        P2{xx, zz} = ls_2Dint(width, f0, mat, kerf, angt, Dt0, x(xx), z(zz), Nopt);
     end
 end
+
+figure(3)
+pcolor(z, x, cellfun(@abs, P2))
+shading interp
+colormap(jet)
+axis image
+grid on
+grid minor
+set(gca,'FontSize',20);
+camroll(-90)
+
+figure(4)
+mesh(z, x, cellfun(@abs, P2))
+shading interp
+colormap(jet)
+axis normal
+grid on
+grid minor
+set(gca,'FontSize',20);
