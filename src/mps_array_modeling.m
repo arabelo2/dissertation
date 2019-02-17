77d93487f69aba27d57a20077c0f5220a1127964
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2019-02-17 20:55:05 -0300

    Update mps_array_modeling

--- src/mps_array_modeling.m
+++ src/mps_array_modeling.m
@@ -2,28 +2,37 @@
 % of rectangular elements radiating waves in a fluid using the MATLAB
 % function ps_3Dv. Both time delay and apodization laws can specified for
 % the array to steer it and focus it.
-clear all
+clear all;
 format longG;
 % --- Give input parameters ---
-lx = 12e-3; % Element length in x-direction
+lx = .5e-3; % Element length in x-direction
 ly = 12e-3; % Element length in y-direction
-gx = 1e-3; % Gap length in x-direction
-gy = 1e-3; % Gap length in y-direction
+gx = .1e-3; % Gap length in x-direction
+gy = .1e-3; % Gap length in y-direction
 f = 2.25e6; % Frequency (Hz)
-c = 1480; % Wave speed (m/s)
-L1 = 1; % Number of elements in x-direction
-L2 = 1; % Number of elements in y-direction
+c = 1500; % Wave speed (m/s)
+L1 = 32; % Number of elements in x-direction
+L2 = 1; % Nuber of elements in y-direction
 theta = 0; % Steering angle in theta direction (deg)
 phi = 0; % Steering angle in phi direction (deg)
-F1 = inf; % Focal distance (m)
+F1 = 40e-3; % Focal distance (m)
 lambda = c/f;
 % Weighting choices are 'rect', 'cos', 'Han', 'Ham', 'Blk', 'tri'.
 ampx_type = 'rect'; % Weighting coefficients in x-direction
 ampy_type = 'rect'; % Weighting coefficients in y-direction
 
 % Field points (x, y, z) to evaluate
-xs = 1e-3*linspace(-15, 15, 1000);
-zs = 1e-3*linspace(0, 70, 1000);
+N = 10;
+xmin = -15e-3;
+xmax = +15e-3;
+xnpoints = N*ceil(abs(xmax - xmin)/lambda);
+xs = linspace(xmin, xmax, xnpoints);
+
+zmin = 0;
+zmax = +120e-3;
+znpoints = N*ceil(abs(zmax - zmin)/lambda);
+zs = linspace(zmin, zmax, znpoints);
+
 y = 0;
 [x, z] = meshgrid(xs, zs);
 % --- End input parameters ---
@@ -46,6 +55,12 @@ for nn = 1:L1
     for mm = 1:L2
         % p = p + Cx(nn)*Cy(mm)*delay(nn, mm)*ps_3Dv(lx, ly, f, c, ex(nn), ey(mm), x, y, z); 
         p = p + Cx*Cy*delay(nn, mm)*ps_3Dv(lx, ly, f, c, ex(nn), ey(mm), x, y, z); 
+        imagesc(xs, zs, abs(p))
+        shading interp
+        colormap(jet)
+        colorbar
+        axis vis3d
+        pause(.05)
     end
 end
 %--- Output ---
