29aeb820f40c6486a173a953380e86d5bd7c9613
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2019-04-15 08:16:03 -0300

    Update files

--- src/mps_array_modeling.m
+++ src/mps_array_modeling.m
@@ -6,16 +6,16 @@ clear all;
 format longG;
 % --- Give input parameters ---
 lx = .15e-3; % Element length in x-direction
-ly = .15e-3; % Element length in y-direction
+ly = 12e-3; % Element length in y-direction
 gx = .05e-3; % Gap length in x-direction
 gy = .05e-3; % Gap length in y-direction
 f = 5e6; % Frequency (Hz)
-c = 1500; % Wave speed (m/s)
-L1 = 11; % Number of elements in x-direction
-L2 = 11; % Nuber of elements in y-direction
-theta = 20; % Steering angle in theta direction (deg)
+c = 1480; % Wave speed (m/s)
+L1 = 32; % Number of elements in x-direction
+L2 = 1; % Nuber of elements in y-direction
+theta = 0; % Steering angle in theta direction (deg)
 phi = 0; % Steering angle in phi direction (deg)
-F1 = 50.4e-3; % Focal distance (m)
+F1 = 20e-3; % Focal distance (m)
 lambda = c/f;
 % Weighting choices are 'rect', 'cos', 'Han', 'Ham', 'Blk', 'tri'.
 ampx_type = 'rect'; % Weighting coefficients in x-direction
@@ -23,24 +23,26 @@ ampy_type = 'rect'; % Weighting coefficients in y-direction
 
 % Field points (x, y, z) to evaluate
 N = 10;
-xmin = -10e-3;
-xmax = +10e-3;
-xnpoints = 1000; % xnpoints = N*ceil(abs(xmax - xmin)/lambda);
+xmin = -25e-3;
+xmax = +25e-3;
+xnpoints = 600; % xnpoints = N*ceil(abs(xmax - xmin)/lambda);
 xs = linspace(xmin, xmax, xnpoints);
 
-% zmin = 0;
-% zmax = +120e-3;
-% znpoints = 300; % znpoints = N*ceil(abs(zmax - zmin)/lambda);
-% zs = linspace(zmin, zmax, znpoints);
+zmin = 1e-3;
+zmax = +50e-3;
+znpoints = 600; % znpoints = N*ceil(abs(zmax - zmin)/lambda);
+zs = linspace(zmin, zmax, znpoints);
+y = 0;
 
-ymin = -10e-3;
-ymax = +10e-3;
-ynpoints = 1000; % ynpoints = N*ceil(abs(ymax - ymin)/lambda);
-ys = linspace(ymin, ymax, ynpoints);
+% % ymin = -25e-3;
+% % ymax = +25e-3;
+% % ynpoints = 300; % ynpoints = N*ceil(abs(ymax - ymin)/lambda);
+% % ys = linspace(ymin, ymax, ynpoints);
+% % z = 15e-3;
 
-z = 4e-3;
-% [x, z] = meshgrid(xs, zs);
-[x, y] = meshgrid(xs, ys);
+[x, z] = meshgrid(xs, zs);
+% %[x, y] = meshgrid(xs, ys);
+
 % --- End input parameters ---
 % Calculate array pitches
 sx = lx + gx;
@@ -53,17 +55,18 @@ ey = (2*Ny - 1 - L2)*sy/2;
 % Generate time delays, put in exponential and calculate amplitude weights
 td = delay_laws3D(L1, L2, sx, sy, theta, phi, F1, c);
 delay = exp(1i.*2.*pi.*f.*td);
-Cx = 1; % Cx = discrete_windows(L1, ampx_type);
-Cy = 1; % Cy = discrete_windows(L2, ampy_type);
+Cx = discrete_windows(L1, ampx_type);
+Cy = discrete_windows(L2, ampy_type);
 % Calculate normalized pressure
 p = 0;
 for nn = 1:L1
     for mm = 1:L2
-        % p = p + Cx(nn)*Cy(mm)*delay(nn, mm)*ps_3Dv(lx, ly, f, c, ex(nn), ey(mm), x, y, z); 
-        p = p + Cx*Cy*delay(nn, mm)*ps_3Dv(lx, ly, f, c, ex(nn), ey(mm), x, y, z); 
-        imagesc(xs, ys, abs(p))
+        p = p + Cx(nn)*Cy(mm)*delay(nn, mm)*ps_3Dv(lx, ly, f, c, ex(nn), ey(mm), x, y, z); 
+        imagesc(xs, zs, abs(p))
+        % % p = p + Cx*Cy*delay(nn, mm)*ps_3Dv(lx, ly, f, c, ex(nn), ey(mm), x, y, z); 
+        % % imagesc(xs, ys, abs(p))
         shading interp
-        colormap(gray) % colormap(jet)
+        colormap(jet) % colormap(gray)
         colorbar
         axis vis3d
         pause(.05)
@@ -71,18 +74,18 @@ for nn = 1:L1
 end
 %--- Output ---
 % Plot results based on specifications of (x, y, z) points
-% imagesc(xs, zs, abs(p))
-% shading interp
-% colormap(jet)
-% colorbar
-% axis vis3d
-
-% Plot results based on specifications of (x, y, z) points
-imagesc(xs, ys, abs(p))
+imagesc(xs, zs, abs(p))
 shading interp
-colormap(gray) % colormap(gray)
+colormap(jet)
 colorbar
 axis vis3d
+
+% Plot results based on specifications of (x, y, z) points
+% % imagesc(xs, ys, abs(p))
+% % shading interp
+% % colormap(gray) % colormap(gray)
+% % colorbar
+% % axis vis3d
 
 % Reference
 % Jr, Lester W. Schmerr. Fundamentals of Ultrasonic Phased Arrays. Solid Mechanics and Its Applications. Springer International Publishing, 2015. //www.springer.com/us/book/9783319072715.
--- src/mps_array_modeling_int.m
+++ src/mps_array_modeling_int.m
@@ -6,7 +6,7 @@ clear all;
 format longG;
 % --- Give input parameters ---
 tic     % Time the calculation
-lx = .25e-3/2;    % Element length in x-direction
+lx = .25e-3;    % Element length in x-direction
 ly = 12e-3;    % Element length in y-direction
 gx = .05e-3;    % Gap length in x-direction
 gy = .05e-3;    % Gap length in y-direction
@@ -18,13 +18,13 @@ cp2 = 5900; % Compressional wave speed, medium two (m/s)
 cs2 = 3200; % Shear wave speed, medium two (m/s)
 type = 'p'; % Wave type, medium two
 mat = {d1, cp1, d2, cp2, cs2, type};    % Form material vector
-L1 = 16;    % Number of elements in x-direction
+L1 = 32;    % Number of elements in x-direction
 L2 = 1; % Nuber of elements in y-direction
 angt = 0;  % Angle of the array (deg)
-Dt0 = 10e-3; % Height of array center from interface (m)
-theta2 = 0;  % Steering angle in theta direction (deg)
+Dt0 = 50e-3; % Height of array center from interface (m)
+theta2 = 10;  % Steering angle in theta direction (deg)
 phi = 0;    % Steering angle in phi direction (deg)
-DF = 10e-3;   % Focal distance (m)
+DF = 100e-3;   % Focal distance (m)
 lambda = mat{2}/f;
 % Weighting choices are 'rect', 'cos', 'Han', 'Ham', 'Blk', 'tri'.
 ampx_type = 'rect'; % Weighting coefficients in x-direction
@@ -32,14 +32,14 @@ ampy_type = 'rect'; % Weighting coefficients in y-direction
 
 % Field points (x, y, z) to evaluate
 N = 10;
-xmin = -15e-3;
-xmax = 15e-3;
+xmin = -50e-3;
+xmax = 50e-3;
 xnpoints = 300; % xnpoints = N*ceil(abs(xmax - xmin)/lambda);
 xs = linspace(xmin, xmax, xnpoints);
 
 zmin = 1e-3;
-zmax = 50e-3;
-znpoints = 300; %znpoints = N*ceil(abs(zmax - zmin)/lambda);
+zmax = 200e-3;
+znpoints = 200; %znpoints = N*ceil(abs(zmax - zmin)/lambda);
 zs = linspace(zmin, zmax, znpoints);
 
 y = 0;
@@ -72,7 +72,7 @@ vy = 0;
 vz = 0;
 for nn = 1:L1
     for ll = 1:L2
-        [vxe, vye, vze] = ps_3Dint(lx, ly, f, mat, ex(nn), ey(ll), angt, Dt0, x, y, z, 1, 1);
+        [vxe, vye, vze] = ps_3Dint(lx, ly, f, mat, ex(nn), ey(ll), angt, Dt0, x, y, z, 1, 1);        
         vx = vx + Cx(nn)*Cy(ll)*delay(nn, ll)*vxe;
         vy = vy + Cx(nn)*Cy(ll)*delay(nn, ll)*vye;
         vz = vz + Cx(nn)*Cy(ll)*delay(nn, ll)*vze;
