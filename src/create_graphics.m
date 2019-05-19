f395e5d8979abef2e093aff985c28b5793f03e40
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2019-05-19 10:08:22 -0300

    Update create_graphics

--- src/create_graphics.m
+++ src/create_graphics.m
@@ -1,34 +1,43 @@
 max_Ppp_r = max(max(Ppp_r));
 max_Ppp_c = max(max(Ppp_c));
+wait = .3;
+close all
 hold on
-plot(x, Ppp_r(:, length(z(z<=.05)))/ max_Ppp_r, 'o')
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.05)))/ max_Ppp_r)
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.05)))/ max_Ppp_c, '+')
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.05)))/ max_Ppp_c)
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.20)))/ max_Ppp_r, 'o')
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.20)))/ max_Ppp_r)
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.20)))/ max_Ppp_c, '+')
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.20)))/ max_Ppp_c)
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.30)))/ max_Ppp_r, 'o')
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.30)))/ max_Ppp_r)
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.30)))/ max_Ppp_c, '+')
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.30)))/ max_Ppp_c)
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.40)))/ max_Ppp_r, 'o')
-pause(2)
-plot(x, Ppp_r(:, length(z(z<=.40)))/ max_Ppp_r)
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.40)))/ max_Ppp_c, '+')
-pause(2)
-plot(x, Ppp_c(:, length(z(z<=.40)))/ max_Ppp_c)
+for index = (300:5:400)/1000
+   zp = length(z(z <= index));
+   plot(x, Ppp_r(:, zp)/ max_Ppp_r, '+')
+   pause(wait)
+   plot(x, Ppp_r(:, zp)/ max_Ppp_r)
+   pause(wait)
+   plot(x, Ppp_c(:, zp)/ max_Ppp_c, 'o')
+   pause(wait)
+   plot(x, Ppp_c(:, zp)/ max_Ppp_c)
+   pause(wait)
+end
+hold off
+
+wait = 5;
+for index = (0)/1000
+   xp = length(x(x <= index));
+   %plot(z, Ppp_r(xp, :)/ max_Ppp_r, '+')
+   %pause(wait)
+   plot(z*1000, Ppp_r(xp, :)/ max_Ppp_r)
+   pause(wait)
+   %plot(z, Ppp_r(xp, :)/ max_Ppp_c, 'o')
+   %pause(wait)
+   plot(z*1000, Ppp_r(xp, :)/ max_Ppp_c)
+   pause(wait)
+end
+hold off
+grid on
+grid minor
+
+a_r=diff(Ppp_r/max_Ppp_r);
+a_c=diff(Ppp_c/max_Ppp_c);
+dist=norm(a_r - a_c);
+tol=0.1;
+if dist<tol
+   disp('graphs have similar charact.');
+else
+   disp('graphs have differente charact.');
+end
