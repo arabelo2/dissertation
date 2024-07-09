0c1154a5627e36b4dedabe6f75f9dfbb024625f8
arabelo2 <rabelo_alexandre@hotmail.com>
2024-07-09 18:10:57 -0300

    Initiate parameters and create function

---
+++ src/py/w2_p2.py
@@ -0,0 +1,15 @@
+# Import libraries
+import numpy as np
+from math import *
+import matplotlib.pyplot as plt
+
+# Initiate parameters
+xmax = 10.0                     # physical domain (m)
+nx = 100                        # number of space sample
+a = 0.25                        # exponent of Gaussian function
+dx = xmax/(nx - 1)              # grid spacing dx (m)
+x0 = xmax/2                     # center of Gaussian function x0 (m)
+x = np.linspace(0, xmax, nx)    # defining space variable
+
+# Initialization of Gaussian function
+f = (1./sqrt(2*np.py*a))*np.exp(-(((x - x0)**2)/(2*a)))
